4807b2496f92bc62e266579aacfce9feb6df20c0
wuqi <908380434@qq.com>
2017-08-03 22:44:11 +0800

    websocket

---
+++ public/main.js
@@ -0,0 +1,69 @@
+/*
+var app = {
+  init: function(){
+
+  },
+
+  bind: function(){
+
+  }
+
+}
+
+app.init()
+
+*/
+
+
+var socket = io()     //直接调用了io,可监听绑定发布事件
+
+
+if(localStorage.name){
+  document.querySelector('footer .name').innerText = localStorage.name
+}
+
+
+socket.on('message', function(data) {
+  console.log('server send...')
+  console.log(data)
+  addMessage(data.name, data.message)
+})
+
+
+document.querySelector('footer input').onchange = function(){
+  var name = localStorage.name || '游客'
+  var message = this.value
+  addMessage(name, message)
+  this.value = ''
+  socket.emit('message', {
+    name: name,
+    message: message
+  })
+}
+
+document.querySelector('footer .name').onblur = function(e){
+  console.log(this.value)
+  if(this.innerText == ''){
+    alert('名字不能为空')
+    this.innerText = localStorage.name || '游客'
+    return 
+  }
+  localStorage.name = this.innerText
+}
+
+
+function addMessage(name, msg){
+  var msgNode = document.createElement('div')
+  var childSpan = document.createElement('span')
+  childSpan.innerText = name + '说:'
+  var childText = document.createTextNode(msg)
+  msgNode.appendChild(childSpan)
+  msgNode.appendChild(childText)
+  msgNode.classList.add('message')
+  setTimeout(function(){
+    msgNode.classList.add('normal')
+  }, 1000)
+  document.body.querySelector('main').appendChild(msgNode)
+  console.log(getComputedStyle(msgNode).width)
+}
+
